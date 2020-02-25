3cbbc4b76f728280afd94ee3c6f966a34f5bca34
Olivier Rits <olivier.rits@imec.be>
2020-02-25 14:36:19 +0100

    account is editable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :license_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :license_number])
   end
 end

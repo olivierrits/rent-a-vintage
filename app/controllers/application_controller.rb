c9b594786c1ead168e68abe5ae27bd765910a62a
Olivier Rits <olivier.rits@imec.be>
2020-02-25 13:47:52 +0100

    added account view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # authenticate user only need when actually requesting a ride
+  # wiating for the final name of the methods to be checked
+  # before_action :authenticate_user! only: :home
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb

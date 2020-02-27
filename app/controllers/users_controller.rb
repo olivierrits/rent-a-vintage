53752746b40cedbe480a1f36b1fd36f5daf64ed7
Thibaut Moreau <thibaut.moreau.massart@gmail.com>
2020-02-27 17:57:17 +0100

    Did partly the dashboard but struggle to finish it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
   def dashboard
     @user = current_user
     @cars = Car.where(user: @user)
+    @rides = Ride.where(user: @user)
+    # @start_date = params[:start_date].to_date
   end
 end
